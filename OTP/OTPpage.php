823e2c9043a4ae960e0311cb25d98c1d58c33b7c
Aniekwe Joshua <joshuaaniekwe@gmail.com>
2017-10-26 23:32:17 +0100

    check the shit

---
+++ OTP/OTPpage.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>OTP Page</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+	<link rel="stylesheet"  href="css/OTPpage.css">
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-offset-3 col-sm-6 otp-rectangle-box" >
+                <div class="otp-text">
+                    <h4><b>ENTER OTP</b></h4>
+                    <P class="otp-p">A One Time Password has been sent to your bank mobile phone number</P>
+                </div>
+
+                <form action="" method="">
+                    <div class="form-group row">
+                        <div class="col-sm-offset-1 col-sm-10">
+                            <input type="text" class="form-control otp-textbox" id="">
+                        </div>
+                    </div>
+
+                    <button type="submit" class="otp-button btn btn-primary btn-lg">SEND</button>
+                </form>
+
+                <div class="otp-resend">
+                    <P class="">OTP not sent?</P>
+                    <button type="button" class="btn btn-default btn-lg">RESEND</button>
+                </div>
+
+
+
+
+
+
+
+
+
+
+
+                <!--<div class="row">
+                    <div class="col-sm-2 col-md-3 "></div>
+                    <div class="col-sm-8 col-md-6 otp-text-center">
+
+
+
+                              <div class="form-group otp-textbox">
+                         <input type="text" class="form-control" id="#" placeholder="">
+                         </div>
+
+                         <p class="otp-p">
+                        <button type="button" class="btn btn-primary btn-lg otp-button">SEND</button>
+                        </p>
+
+
+
+                    <div class="col-sm-2 col-md-3">
+
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="text-center">
+
+                        </div>
+                    </div>
+
+                </div>-->
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+
+
+
